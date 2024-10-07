9dc6f2b79818c1382b9a8a3e74016cde376bd2e8
Clay O'Dell <clay.odell.music@gmail.com>
2024-10-07 16:54:11 -0500

    final commit for DogList

--- src/DogList.js
+++ src/DogList.js
@@ -5,11 +5,13 @@ function DogList({ dogs }) {
     return (
         <div>
             <h1>Meet The Dogs</h1>
+            <nav>
             <ul>
                 {dogs.map(dog => (
                     <li key={dog.name}><Link to={`/dogs/${dog.name.toLowerCase()}`}>{dog.name}</Link></li>
                 ))}
             </ul>
+            </nav>
         </div>
     );
 }
